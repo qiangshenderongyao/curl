4a350f5ba98e8f1f43222e3248fc537d6e99ff76
qiangshenderongyao <2302938274@qq.com>
2019-04-09 12:23:54 +0000

    a

--- ce.php
+++ ce.php
@@ -1,10 +1,42 @@
 <?php
+error_reporting(E_ALL);
+//$str='012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789';
+//openssl_public_encrypt($str,$encrypt_data,file_get_contents(__DIR__.'/pub.key'),OPENSSL_PKCS1_PADDING);
+function rsaEncrypt($str){
+    $i=0;
+    $all='';
+    while ($sub_str=substr($str,$i,117)){
+       openssl_public_encrypt($sub_str,$encrypt_data,file_get_contents(__DIR__.'/pub.key'),OPENSSL_PKCS1_PADDING);
+       $all .=base64_encode($encrypt_data);
+       $i+=117;
+    }
+    return $all;
+}
+function rsaDecrypt($encrypt_str){
+    $i=0;
+    $all='';
+    while ($sub_str=substr($encrypt_str,$i,172)){
+        $decode_data=base64_decode($sub_str);
+        openssl_public_decrypt($decode_data,$encrypt_data,file_get_contents(__DIR__.'/pub.key'),OPENSSL_PKCS1_PADDING);
+        $all .=$encrypt_data;
+        $i+=172;
+    }
+    return $all;
+}
+//$all=rsaEncrypt($str);
+//$decrypt=rsaDecrypt($all);
+//echo $decrypt;
+//exit;
+//echo base64_encode($encrypt_data);
+//echo '<hr />';
+//echo strlen(base64_encode($encrypt_data));
+//exit;
 $new=time();
 $app_id=md5(1);
 $app_key=md5('594188');
 
-//$url="http://1807.96myshop.cn/ceshi";
-$url="http://96cms.cn/ceshi";
+$url="http://1807.96myshop.cn/ceshi";
+//$url="http://96cms.cn/ceshi";
 $json=[
     'name'=>'枪神',
     'pad'=>'594188',
@@ -14,7 +46,8 @@ $method = 'AES-128-CBC';//加密方法
 $passwd = 'password';//加密密钥
 $salt='xxxxx';
 //$iv=substr(md5($new.$salt),5,16);
-$data=openssl_encrypt(json_encode($json),$method,$passwd,false,'0614668812076688');
+//$data=openssl_encrypt(json_encode($json),$method,$passwd,false,'0614668812076688');
+$data=rsaEncrypt(json_encode($json));
 //数据加密
 $api_param=[];
 $api_param['data']=$data;
@@ -37,13 +70,19 @@ $res=curl_exec($ch);    //接收响应
 //print_r($res);die;
 if(curl_errno($ch)){
     var_dump(curl_errno($ch));
+    var_dump(curl_error());
 }
+//var_dump($res);
+//echo "<hr />";exit;
 $api_arr=json_decode($res,true);
 //var_dump($api_arr);
 //echo '<pre />';
 $date=$api_arr['data'];
-$decrypt_data=openssl_decrypt($date,'AES-128-CBC','password',false,'0614668812076688');
+//var_dump($date);die;
+$decrypt_data=rsaDecrypt($date);
+//var_dump($decrypt_data);die;
 $api_result=json_decode($decrypt_data,true);
+//var_dump($api_result);die;
 ksort($api_result);
 $sign=md5(http_build_query($api_result).'&app_key='.$app_id);
 //var_dump($sign);die;
